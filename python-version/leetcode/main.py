7e564142930606ceeab82d2bab5d283a8c8c4baf
Xuchen <1023578360@qq.com>
2021-09-20 11:36:35 +0800

    Daily Quiz - common 36

--- python-version/leetcode/main.py
+++ python-version/leetcode/main.py
@@ -1,10 +1,11 @@
 import Solution_1877
+import Solution_36
 import Solution_53
 import Solution_138
 from Interview_10_02 import Interview_10_02
 
 if __name__ == '__main__':
-    solution = Solution_138.Solution()
+    solution = Solution_36.Solution()
     solution.main()
     # interview = Interview_10_02()
     # interview.main()
