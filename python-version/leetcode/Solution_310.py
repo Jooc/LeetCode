ce380d86d07e8d38848bdc5b18912a0abbaa0bdb
Xuchen <1023578360@qq.com>
2022-04-06 10:02:38 +0800

    4-6 Common 310: [yet understand]

---
+++ python-version/leetcode/Solution_310.py
@@ -0,0 +1,38 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+
+        g = [[] for _ in range(n)]
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+        parents = [0] * n
+
+        def bfs(start: int):
+            vis = [False] * n
+            vis[start] = True
+            q = deque([start])
+            while q:
+                x = q.popleft()
+                for y in g[x]:
+                    if not vis[y]:
+                        vis[y] = True
+                        parents[y] = x
+                        q.append(y)
+            return x
+
+        x = bfs(0)
+        y = bfs(x)
+
+        path = []
+        parents[x] = -1
+        while y != -1:
+            path.append(y)
+            y = parents[y]
+        m = len(path)
+        return [path[m//2]] if m % 2 else [path[m//2 - 1], path[m // 2]]
