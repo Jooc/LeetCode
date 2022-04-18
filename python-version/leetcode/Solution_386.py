54840fb1926e042ee1e5320cee7133514303912b
Xuchen <1023578360@qq.com>
2022-04-18 09:37:23 +0800

    4-18 Common 386

---
+++ python-version/leetcode/Solution_386.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def main(self):
+        print(self.lexicalOrder(192))
+
+    def lexicalOrder(self, n: int) -> List[int]:
+        ans = [0] * n
+        num = 1
+
+        for i in range(n):
+            ans[i] = num
+            if num * 10 <= n:
+                num *= 10
+            else:
+                while num % 10 == 9 or num + 1 > n:
+                    num //= 10
+                num += 1
+
+        return ans
