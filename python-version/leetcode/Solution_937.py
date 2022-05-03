1e14f0fb75ff859ccfdef5cf3224be28d4c736b8
Xuchen <1023578360@qq.com>
2022-05-03 09:58:11 +0800

    5-3 Common 937: pass

---
+++ python-version/leetcode/Solution_937.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def trans(log: str) -> tuple:
+            a, b = log.split(' ', 1)
+            return (0, b, a) if b[0].isalpha() else (1,)
+
+        logs.sort(key=trans)  # sort 是稳定排序
+        return logs
