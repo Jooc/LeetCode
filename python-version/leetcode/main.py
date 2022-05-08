8822e53a394ad769117927dcccb98d1538a4f8aa
Xuchen <1023578360@qq.com>
2022-05-08 09:43:37 +0800

    edit main

--- python-version/leetcode/main.py
+++ python-version/leetcode/main.py
@@ -1,10 +1,10 @@
-import Solution_720
+from Solution_442 import Solution
 
 from Interview_10_02 import Interview_10_02
 from Top100 import Solution_1
 
 if __name__ == '__main__':
-    solution = Solution_720.Solution()
+    solution = Solution()
     solution.main()
     # interview = Interview_10_02()
     # interview.main()
