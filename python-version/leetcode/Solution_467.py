a0c157e8e805142b2e88ba1a62f8a0f2b3abdd6f
Xuchen <1023578360@qq.com>
2022-05-25 20:14:26 +0800

    5-25 Common 467: [PASS] But seems worthy think

---
+++ python-version/leetcode/Solution_467.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findSubstringInWraproundString(self, p: str) -> int:
+        dp = defaultdict(int)
+        k = 0
+        for i, ch in enumerate(p):
+            if i > 0 and (ord(ch) - ord(p[i - 1])) % 26 == 1:  # 字符之差为 1 或 -25
+                k += 1
+            else:
+                k = 1
+            dp[ch] = max(dp[ch], k)
+        return sum(dp.values())
