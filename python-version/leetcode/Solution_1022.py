a53959c72950d0f2e67ef13f6d59909a488c721e
Xuchen <1023578360@qq.com>
2022-05-30 23:19:34 +0800

    5-30 Common1022: dfs with a val carried

---
+++ python-version/leetcode/Solution_1022.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, val):
+            if node is None:
+                return 0
+            val = (val << 1) | node.val
+            if node.left is None and node.right is None:
+                return val
+            return dfs(node.left, val) + dfs(node.right, val)
+        return dfs(root, 0)
