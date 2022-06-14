ff2bb377e2931552088680ab6774cf919438b94f
Xuchen <1023578360@qq.com>
2022-06-14 08:56:56 +0800

    6-14 Common 498: [Rules Simulation]

---
+++ python-version/leetcode/Solution_498.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        ans = []
+
+        for i in range(m + n - 1):
+            if i % 2:
+               x = 0 if i < n else i - n + 1
+               y = i if i < n else n - 1
+               while x < m and y >= 0:
+                   ans.append(mat[x][y])
+                   x += 1
+                   y -= 1
+            else:
+                x = i if i < m else m - 1
+                y = 0 if i < m else i - m + 1
+                while x >= 0 and y < n:
+                    ans.append(mat[x][y])
+                    x -= 1
+                    y += 1
+        return ans
