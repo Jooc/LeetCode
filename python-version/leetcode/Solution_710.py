d31e315b0145b6e9ac251a8aeeda73083ba355e4
Xuchen <1023578360@qq.com>
2022-06-26 10:51:42 +0800

    6-26 Common 710: Randomly pick while there is blacklist

---
+++ python-version/leetcode/Solution_710.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+
+    def __init__(self, n: int, blacklist: List[int]):
+        m = len(blacklist)
+        self.bound = w = n - m
+        black = [b for b in blacklist if b >= self.bound]
+        self.b2w = {}
+        for b in blacklist:
+            if b < self.bound:
+                while w in black:
+                    w += 1
+                self.b2w[b] = w
+                w += 1
+
+    def pick(self) -> int:
+        x = randrange(self.bound)
+        return self.b2w.get(x, x)
+
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(n, blacklist)
+# param_1 = obj.pick()
