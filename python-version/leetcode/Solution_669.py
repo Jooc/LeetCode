a9e4221999c4144aa62ad33e75d339cb10168697
Xuchen <1023578360@qq.com>
2022-09-10 11:32:59 +0800

    9-10  Common 669: 二叉搜索树剪枝: [Trim BST]

---
+++ python-version/leetcode/Solution_669.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from typing import Optional
+
+
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if root is None:
+            return None
+
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:
+            return self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+        return root
+
+root = TreeNode(1, TreeNode(0), TreeNode(2))
+print(Solution().trimBST(root, 1, 2))
