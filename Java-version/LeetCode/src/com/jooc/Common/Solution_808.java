bd1e494d8a6f4e9ababbfce337a1d4603f3af926
Xuchen <1023578360@qq.com>
2022-11-21 10:47:10 +0800

    11-21 Common 808

---
+++ Java-version/LeetCode/src/com/jooc/Common/Solution_808.java
@@ -0,0 +1,31 @@
+package com.jooc.Common;
+
+import com.jooc.Solution;
+
+public class Solution_808 implements Solution {
+
+    public double soupServings(int n) {
+        n = (int) Math.ceil((double) n / 25);
+        if (n >= 179) {
+            return 1.0;
+        }
+
+        double[][] dp = new double[n + 1][n + 1];
+        dp[0][0] = 0.5;
+
+        for (int i = 1; i <= n; i++){
+            dp[0][i] = 1.0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = (dp[Math.max(0, i - 1)][Math.max(0, j - 3)] + dp[Math.max(0, i - 2)][Math.max(0, j - 2)]
+                        + dp[Math.max(0, i - 3)][Math.max(0, j - 1)] + dp[Math.max(0, i - 4)][j]) / 4.0;
+            }
+        }
+        return dp[n][n];
+    }
+
+    @Override
+    public void main() {
+    }
+}
