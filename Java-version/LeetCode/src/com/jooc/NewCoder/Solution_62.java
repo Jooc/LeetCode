d3947c80afef68b5ffd58540076c1b4c588f0ee7
Xuchen <1023578360@qq.com>
2023-03-01 23:28:30 +0800

    3-1: Basic DP

---
+++ Java-version/LeetCode/src/com/jooc/NewCoder/Solution_62.java
@@ -0,0 +1,26 @@
+package com.jooc.NewCoder;
+
+import com.jooc.Solution;
+
+public class Solution_62 implements Solution {
+    @Override
+    public void main() {
+
+    }
+
+    public int jumpFloor_0(int target) {
+        if(target <= 2) return target;
+        return jumpFloor(target - 1) + jumpFloor(target - 2);
+    }
+
+    public int jumpFloor(int target) {
+        if(target <= 2) return target;
+        int a = 1, b = 2, c = 2;
+        for(int i = 3; i <= target; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
