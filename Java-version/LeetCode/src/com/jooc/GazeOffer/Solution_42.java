341f361de5fb8bb7e66165ac4418133fe4a04728
Xuchen <1023578360@qq.com>
2023-03-21 10:46:15 +0800

    3-21: ByteDance

--- Java-version/LeetCode/src/com/jooc/GazeOffer/Solution_42.java
+++ Java-version/LeetCode/src/com/jooc/GazeOffer/Solution_42.java
@@ -2,24 +2,34 @@ package com.jooc.GazeOffer;
 
 public class Solution_42 {
 
-    public void main(){
-        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    public void main() {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
-    public int maxSubArray(int[] nums) {
+    public int maxSubArray_0(int[] nums) {
         int len = nums.length;
-        if(len == 0) return 0;
+        if (len == 0) return 0;
 
         int[] dp = new int[len];
         dp[0] = nums[0];
 
         int max = dp[0];
-        for(int i = 1; i < len; i++){
-            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+        for (int i = 1; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             max = Math.max(max, dp[i]);
         }
 
         return max;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + Math.max(nums[i - 1], 0);
+            ans = Math.max(ans, nums[i]);
+        }
+        return ans;
     }
 
 }
